75e21071ebf07e28c67fc68a17ff8c1fe6cac669
Eric Pierce <wapcaplet88@gmail.com>
2014-07-17 14:40:18 -0600

    Add some Project specs

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,8 +3,40 @@ require 'rtrail/project'
 
 module RTrail
   describe Project do
-    let(:base_url) { MOCK_APP_URL }
-    it "gets the project data"
+    context "Class methods" do
+      describe "#all" do
+        it "returns all Projects" do
+          projects = Project.all
+          expect(projects).to be_an(Array)
+        end
+      end
+
+      describe "#by_name" do
+        it "returns the Project with the given name if it exists" do
+          project = Project.by_name("First Project")
+          expect(project).to be_a(Project)
+          expect(project).to_not be_nil
+          expect(project.name).to eq("First Project")
+        end
+
+        it "raises RTrailError if no project exists with the given name" do
+          expect {
+            Project.by_name("Bogus Project Name")
+          }.to raise_error(RTrailError, /not found in TestRail/)
+        end
+      end
+    end
+
+    context "Instance methods" do
+      describe "#initialize" do
+        it "gets the project data" do
+          project = Project.new(1)
+          expect(project).to_not be_nil
+          expect(project.id).to eq(1)
+        end
+      end
+    end
+
   end
 end # module RTrail
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,20 @@
+require 'simplecov'
+SimpleCov.start if ENV['COVERAGE']
+
 PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
 SPEC_DIR = File.join(PROJECT_ROOT, 'spec')
 MOCK_APP_URL = "http://localhost:8080/index.php?/api/v2/"
 
 $LOAD_PATH.unshift(File.join(PROJECT_ROOT, 'lib'))
+require 'rtrail/client'
+require 'rtrail/entity'
 
 module SpecHelpers
 end
 
-RSpec.configure do |c|
-  c.include SpecHelpers
+RSpec.configure do |config|
+  config.include SpecHelpers
+  config.before(:all) do
+    RTrail::Entity.client = RTrail::Client.new(MOCK_APP_URL, 'user', 'password')
+  end
 end
